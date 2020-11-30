c0e133c7275613a2b1cc9290967e1a4e87425bfc
jae2tree <jae2tree@gmail.com>
2020-11-30 21:30:12 +0900

    spring boot 기초설정

---
+++ spring-social/src/main/java/com/rest/demo/entity/User.java
@@ -0,0 +1,71 @@
+package com.rest.demo.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @JsonIgnore
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private List<UserAuthority> authorities = new ArrayList<UserAuthority>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<UserAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(List<UserAuthority> authorities) {
+        this.authorities = authorities;
+    }
+}
